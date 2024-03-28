0d76b1f6de57549fcef0c636fb80804ffefdc76a
QuickMash <106212829+QuickMash@users.noreply.github.com>
2024-03-28 16:30:24 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,22 +3,20 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>QuickSearch NET.1</title>
+<title>QuickSearch NET</title>
 </head>
 <body>
-<h1>QuickSearch</h1>
-<h2>By QuickMash Studios</h2>
+<h1>QuickSearch NET</h1>
+<p>By QuickMash Studios</p>
 <div class="focus">
 <form method="GET" action="result.php">
-<label for="query">Search:</label>
-<input type="text" id="query" name="query" required>
+<input type="text" id="query" name="query" placeholder="Search QuickSearch" maxlength="50" required>
 <button type="submit">Search</button>
 </form>
 </div>
 <footer>
-<p>QuickMash Studios QuickSearch Model NET.1 Alpha Plus</p>
-<a href="https://wiki.quickmashstudios.com/">WIKI</a>
-<p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://github.com/QuickMash/QSNET.1">QuickSearch</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://youtube.com/channels/@QUICKMASH">Tanner Curr</a> is licensed under <a href="http://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"></a></p>
+<p>QuickSearch © 2023 by Tanner Curr is licensed under CC BY-SA 4.0 | QuickSearch NET</p>
+<p>Amazing Emojis from <a href="https://openmoji.org/" >OpenMoji</a></p>
 </footer>
 </body>
 </html>
